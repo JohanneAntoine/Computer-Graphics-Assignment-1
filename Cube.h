6797493b4710836e31f10576b74ac35bace5b9dc
Ian Hackman <119762410+IanHackman@users.noreply.github.com>
2024-09-28 01:04:46 -0400

    Cube Fully Implemented (Draw and Normals)

--- Cube.h
+++ Cube.h
@@ -18,157 +18,166 @@ public:
 		float stepY = 1.0f / m_segmentsY;
 
 		// front face
-		for (int i = 0; i < m_segmentsX; i++) {
-			for (int j = 0; j < m_segmentsY; j++) {
-				float x0 = -0.5f + i * stepX;
-				float x1 = -0.5f + (i + 1) * stepX;
-				float y0 = -0.5f + j * stepY;
-				float y1 = -0.5f + (j + 1) * stepY;
-
-				glBegin(GL_TRIANGLES);
-
-				glNormal3f(0.0f, 0.0f, 1.0f);
-
-				glVertex3f(x0, y0, 0.5f);
-				glVertex3f(x1, y0, 0.5f);
-				glVertex3f(x1, y1, 0.5f);
-
-				glVertex3f(x0, y0, 0.5f);
-				glVertex3f(x1, y1, 0.5f);
-				glVertex3f(x0, y1, 0.5f);
-
-				glEnd();
-			}
-		}
+		draw_face(stepX, stepY, 0.5f, 0, 0, 1.0f);
 
 		// back face
-		for (int k = 0; k < m_segmentsX; k++) {
-			for (int l = 0; l < m_segmentsY; l++) {
-				float x0 = -0.5f + k * stepX;
-				float x1 = -0.5f + (k + 1) * stepX;
-				float y0 = -0.5f + l * stepY;
-				float y1 = -0.5f + (l + 1) * stepY;
-
-				glBegin(GL_TRIANGLES);
-
-				glNormal3f(0.0f, 0.0f, -1.0f);
-
-				glVertex3f(x0, y0, -0.5f);
-				glVertex3f(x1, y0, -0.5f);
-				glVertex3f(x1, y1, -0.5f);
-
-				glVertex3f(x0, y0, -0.5f);
-				glVertex3f(x1, y1, -0.5f);
-				glVertex3f(x0, y1, -0.5f);
-
-				glEnd();
-			}
-		}
+		draw_face(stepX, stepY, -0.5f, 0, 0, -1.0f);
 
 		// right face
-		for (int m = 0; m < m_segmentsX; m++) {
-			for (int n = 0; n < m_segmentsY; n++) {
-				float y0 = -0.5f + m * stepX;
-				float y1 = -0.5f + (m + 1) * stepX;
-				float z0 = -0.5f + n * stepY;
-				float z1 = -0.5f + (n + 1) * stepY;
-
-				glBegin(GL_TRIANGLES);
-
-				glNormal3f(1.0f, 0.0f, 0.0f);
-
-				glVertex3f(0.5f, y0, z0);
-				glVertex3f(0.5f, y1, z0);
-				glVertex3f(0.5f, y1, z1);
-
-				glVertex3f(0.5f, y0, z0);
-				glVertex3f(0.5f, y1, z1);
-				glVertex3f(0.5f, y0, z1);
-
-				glEnd();
-			}
-		}
+		draw_face(stepX, stepY, 0.5f, 1.0f, 0, 0);
 
 		// left face
-		for (int o = 0; o < m_segmentsX; o++) {
-			for (int p = 0; p < m_segmentsY; p++) {
-				float y0 = -0.5f + o * stepX;
-				float y1 = -0.5f + (o + 1) * stepX;
-				float z0 = -0.5f + p * stepY;
-				float z1 = -0.5f + (p + 1) * stepY;
-
-				glBegin(GL_TRIANGLES);
-
-				glNormal3f(-1.0f, 0.0f, 0.0f);
-
-				glVertex3f(-0.5f, y0, z0);
-				glVertex3f(-0.5f, y1, z0);
-				glVertex3f(-0.5f, y1, z1);
-
-				glVertex3f(-0.5f, y0, z0);
-				glVertex3f(-0.5f, y1, z1);
-				glVertex3f(-0.5f, y0, z1);
-
-				glEnd();
-			}
-		}
+		draw_face(stepX, stepY, -0.5f, -1.0f, 0, 0);
 
 		// top face
-		for (int q = 0; q < m_segmentsX; q++) {
-			for (int r = 0; r < m_segmentsY; r++) {
-				float x0 = -0.5f + q * stepX;
-				float x1 = -0.5f + (q + 1) * stepX;
-				float z0 = -0.5f + r * stepY;
-				float z1 = -0.5f + (r + 1) * stepY;
-
-				glBegin(GL_TRIANGLES);
-
-				glNormal3f(0.0f, 1.0f, 0.0f);
-
-				glVertex3f(x0, 0.5f, z0);
-				glVertex3f(x1, 0.5f, z0);
-				glVertex3f(x1, 0.5f, z1);
-
-				glVertex3f(x0, 0.5f, z0);
-				glVertex3f(x1, 0.5f, z1);
-				glVertex3f(x0, 0.5f, z1);
-
-				glEnd();
-			}
-		}
+		draw_face(stepX, stepY, 0.5f, 0, 1.0f, 0);
 
 		// bottom face
-		for (int s = 0; s < m_segmentsX; s++) {
-			for (int t = 0; t < m_segmentsY; t++) {
-				float x0 = -0.5f + s * stepX;
-				float x1 = -0.5f + (s + 1) * stepX;
-				float z0 = -0.5f + t * stepY;
-				float z1 = -0.5f + (t + 1) * stepY;
-
-				glBegin(GL_TRIANGLES);
-
-				glNormal3f(0.0f, -1.0f, 0.0f);
-
-				glVertex3f(x0, -0.5f, z0);
-				glVertex3f(x1, -0.5f, z0);
-				glVertex3f(x1, -0.5f, z1);
-
-				glVertex3f(x0, -0.5f, z0);
-				glVertex3f(x1, -0.5f, z1);
-				glVertex3f(x0, -0.5f, z1);
-
-				glEnd();
-			}
-		}
+		draw_face(stepX, stepY, -0.5f, 0, -1.0f, 0);
 
 	};
 
 
 	void drawNormal() {
+		float stepX = 1.0f / m_segmentsX;
+		float stepY = 1.0f / m_segmentsY;
+
+		// front face
+		draw_normal_face(stepX, stepY, 0.5f, 0, 0, 1.0f);
+
+		// back face
+		draw_normal_face(stepX, stepY, -0.5f, 0, 0, -1.0f);
+
+		// right face
+		draw_normal_face(stepX, stepY, 0.5f, 1.0f, 0, 0);
+
+		// left face
+		draw_normal_face(stepX, stepY, -0.5f, -1.0f, 0, 0);
+
+		// top face
+		draw_normal_face(stepX, stepY, 0.5f, 0, 1.0f, 0);
+
+		// bottom face
+		draw_normal_face(stepX, stepY, -0.5f, 0, -1.0f, 0);
 
 	};
 
 private:
+	void draw_face(float stepX, float stepY, float fixedCoordinate, float normalX, float normalY, float normalZ) {
+
+		glBegin(GL_TRIANGLES);
+
+		glNormal3f(normalX, normalY, normalZ);
+
+		for (int i = 0; i < m_segmentsX; i++) {
+			for (int j = 0; j < m_segmentsY; j++) {
+				float v0 = -0.5f + i * stepX;
+				float v1 = -0.5f + (i + 1) * stepX;
+				float w0 = -0.5f + j * stepY;
+				float w1 = -0.5f + (j + 1) * stepY;
+
+				// right or left
+				if (normalX == 1.0f || normalX == -1.0f) {
+					glVertex3f(fixedCoordinate, w0, v0);
+					glVertex3f(fixedCoordinate, w1, v0);
+					glVertex3f(fixedCoordinate, w1, v1);
+
+					glVertex3f(fixedCoordinate, w0, v0);
+					glVertex3f(fixedCoordinate, w1, v1);
+					glVertex3f(fixedCoordinate, w0, v1);
+				}
+
+				// top or bottom
+				else if (normalY == 1.0f || normalY == -1.0f) {
+					glVertex3f(v0, fixedCoordinate, w0);
+					glVertex3f(v1, fixedCoordinate, w0);
+					glVertex3f(v1, fixedCoordinate, w1);
+
+					glVertex3f(v0, fixedCoordinate, w0);
+					glVertex3f(v1, fixedCoordinate, w1);
+					glVertex3f(v0, fixedCoordinate, w1);
+				}
+
+				// front or back
+				else if (normalZ == 1.0f || normalZ == -1.0f) {
+					glVertex3f(v0, w0, fixedCoordinate);
+					glVertex3f(v1, w0, fixedCoordinate);
+					glVertex3f(v1, w1, fixedCoordinate);
+
+					glVertex3f(v0, w0, fixedCoordinate);
+					glVertex3f(v1, w1, fixedCoordinate);
+					glVertex3f(v0, w1, fixedCoordinate);
+				}
+			}
+		}
+
+		glEnd();
+	}
+
+	void draw_normal_face(float stepX, float stepY, float fixedCoordinate, float normalX, float normalY, float normalZ) {
+		float halfsize = 0.5f;
+		float normalLineLength = 0.5f;
+
+		glColor3f(0.0f, 1.0f, 0.0f);
+
+		for (int i = 0; i < m_segmentsX; i++) {
+			for (int j = 0; j < m_segmentsY; j++) {
+				float v0 = -halfsize + i * stepX;
+				float v1 = -halfsize + (i + 1) * stepX;
+				float w0 = -halfsize + j * stepY;
+				float w1 = -halfsize + (j + 1) * stepY;
+
+				glBegin(GL_LINES);
+
+				// right or left
+				if (normalX == 1.0f || normalX == -1.0f) {
+					glVertex3f(fixedCoordinate, w0, v0);
+					glVertex3f(fixedCoordinate + normalX * normalLineLength, w0, v0);
+
+					glVertex3f(fixedCoordinate, w1, v0);
+					glVertex3f(fixedCoordinate + normalX * normalLineLength, w1, v0);
+
+					glVertex3f(fixedCoordinate, w1, v1);
+					glVertex3f(fixedCoordinate + normalX * normalLineLength, w1, v1);
+
+					glVertex3f(fixedCoordinate, w0, v1);
+					glVertex3f(fixedCoordinate + normalX * normalLineLength, w0, v1);
+				}
+
+				// top or bottom
+				else if (normalY == 1.0f || normalY == -1.0f) {
+					glVertex3f(v0, fixedCoordinate, w0);
+					glVertex3f(v0, fixedCoordinate + normalY * normalLineLength, w0);
+
+					glVertex3f(v1, fixedCoordinate, w0);
+					glVertex3f(v1, fixedCoordinate + normalY * normalLineLength, w0);
+
+					glVertex3f(v1, fixedCoordinate, w1);
+					glVertex3f(v1, fixedCoordinate + normalY * normalLineLength, w1);
+
+					glVertex3f(v0, fixedCoordinate, w1);
+					glVertex3f(v0, fixedCoordinate + normalY * normalLineLength, w1);
+				}
+
+				// front or back
+				else if (normalZ == 1.0f || normalZ == -1.0f) {
+					glVertex3f(v0, w0, fixedCoordinate);
+					glVertex3f(v0, w0, fixedCoordinate + normalZ * normalLineLength);
+
+					glVertex3f(v1, w0, fixedCoordinate);
+					glVertex3f(v1, w0, fixedCoordinate + normalZ * normalLineLength);
+
+					glVertex3f(v1, w1, fixedCoordinate);
+					glVertex3f(v1, w1, fixedCoordinate + normalZ * normalLineLength);
+
+					glVertex3f(v0, w1, fixedCoordinate);
+					glVertex3f(v0, w1, fixedCoordinate + normalZ * normalLineLength);
+				}
+
+				glEnd();
+			}
+		}
+	}
 };
 
 #endif
